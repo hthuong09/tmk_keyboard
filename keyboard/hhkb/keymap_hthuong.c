712616a8cd0a4f0ec3ba6c25c02218ec2ea27ede
Thuong Nguyen <thuongnguyen.net@gmail.com>
2017-05-25 22:44:50 +0700

    Remake layer

--- keyboard/hhkb/keymap_hthuong.c
+++ keyboard/hhkb/keymap_hthuong.c
@@ -1,5 +1,5 @@
 /*
- * hthuong's HHKB keymap
+ * hthuong's HHKB
  */
 #include "keymap_common.h"
 
@@ -9,11 +9,7 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] __attribute__ ((section (".key
 #else
 const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
 #endif
-    /* Layer 0: Default Layer
-     * Modified from normal:
-     *   Escape = `
-     *   Control = Escape on click and control on hold
-     *   Swapped Gui and Alt
+    /* Layer 0: Normal mode
      * ,-----------------------------------------------------------.
      * |`  |  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|  \|Del|
      * |-----------------------------------------------------------|
@@ -23,76 +19,145 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
      * |-----------------------------------------------------------|
      * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift |Fn0|
      * `-----------------------------------------------------------'
-     *       |Gui|Alt  |         Space         |Alt  |Gui|
+     *       |Alt|Gui  |         Space         |Gui  |Alt|
      *       `-------------------------------------------'
      */
     [0] = \
-    KEYMAP(GRV, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSLS,DEL,   \
+    KEYMAP(GRV, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSLS, DEL,   \
            TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSPC,       \
-           FN1 ,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,ENT,             \
-           LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,RSFT,FN0,             \
-                TGUI,LLAL,          SPC,                RALT,RGUI),
+           FN1, A,   S,   D, FN4,   G,   H,   J,   K,   L,   SCLN,QUOT,ENT,             \
+           FN2,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH, FN3,FN0,             \
+                LALT,LGUI,          SPC,                RGUI,RALT),
 
-};
+    /* Layer 1: Fn mode
+     * ,-----------------------------------------------------------.
+     * |Pwr| F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|Ins|Del|
+     * |-----------------------------------------------------------|
+     * |Caps |   |   |   |   |   |   |   |Psc|Slk|Pus|Up |   |Backs|
+     * |-----------------------------------------------------------|
+     * |      |VoD|VoU|Mut|   |   |  *|  /|Hom|PgU|Lef|Rig|Enter   |
+     * |-----------------------------------------------------------|
+     * |        |   |   |   |   |   |  +|  -|End|PgD|Dow|      |   |
+     * `-----------------------------------------------------------'
+     *       |   |     |                       |     |   |
+     *       `-------------------------------------------'
+     */
+    [1] = \
+    KEYMAP(PWR, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, INS, DEL,   \
+           CAPS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,PSCR,SLCK,PAUS, UP, TRNS, BSPC,      \
+           TRNS,VOLD,VOLU,MUTE,TRNS,TRNS,PAST,PSLS,HOME,PGUP,LEFT,RGHT,PENT,            \
+           TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,PPLS,PMNS,END, PGDN,DOWN,TRNS,TRNS,            \
+                TRNS,TRNS,          TRNS,               TRNS,TRNS),
 
-
-
-/*
- * Fn action definition
- */
-#ifdef KEYMAP_SECTION_ENABLE
-const action_t fn_actions[] __attribute__ ((section (".keymap.fn_actions"))) = {
-#else
-const action_t fn_actions[] PROGMEM = {
-#endif
-    [0]  = ACTION_LAYER_MOMENTARY(1),
-    [1]  = ACTION_MODS_TAP_KEY(MOD_LCTL, KC_ESC),
-    //[0]  = ACTION_LAYER_MOMENTARY(1),
-    //[1]  = ACTION_LAYER_MOMENTARY(2),
-    //[2]  = ACTION_LAYER_MOMENTARY(3),
-    //[3]  = ACTION_LAYER_MOMENTARY(4),
-    //[4]  = ACTION_LAYER_MOMENTARY(5),
-    //[5]  = ACTION_LAYER_MOMENTARY(6),
-    //[6]  = ACTION_LAYER_MOMENTARY(7),
-    //[7]  = ACTION_LAYER_TOGGLE(1),
-    //[8]  = ACTION_LAYER_TOGGLE(2),
-    //[9]  = ACTION_LAYER_TOGGLE(3),
-    //[10] = ACTION_LAYER_TAP_TOGGLE(1),
-    //[11] = ACTION_LAYER_TAP_TOGGLE(2),
-    //[12] = ACTION_LAYER_TAP_TOGGLE(3),
-    //[13] = ACTION_LAYER_TAP_KEY(1, KC_BSLASH),
-    //[14] = ACTION_LAYER_TAP_KEY(2, KC_TAB),
-    //[15] = ACTION_LAYER_TAP_KEY(3, KC_ENTER),
-    //[16] = ACTION_LAYER_TAP_KEY(4, KC_SPACE),
-    //[17] = ACTION_LAYER_TAP_KEY(5, KC_SCOLON),
-    //[18] = ACTION_LAYER_TAP_KEY(6, KC_QUOTE),
-    //[19] = ACTION_LAYER_TAP_KEY(7, KC_SLASH),
-    //[20] = ACTION_MODS_TAP_KEY(MOD_LSFT, KC_SPACE),
-    //[21] = ACTION_MODS_TAP_KEY(MOD_LCTL, KC_SPACE),
-    //[22] = ACTION_MODS_TAP_KEY(MOD_RCTL, KC_QUOTE),
-    //[23] = ACTION_MODS_TAP_KEY(MOD_RCTL, KC_ENTER),
-    //[24] = ACTION_MODS_TAP_KEY(MOD_LCTL, KC_ESC),
-    //[25] = ACTION_MODS_TAP_KEY(MOD_LCTL, KC_BSPACE),
-    //[26] = ACTION_MODS_ONESHOT(MOD_LCTL),
-    //[27] = ACTION_MODS_TAP_KEY(MOD_LSFT, KC_ESC),
-    //[28] = ACTION_MODS_TAP_KEY(MOD_LSFT, KC_BSPACE),
-    //[29] = ACTION_MODS_ONESHOT(MOD_LSFT),
-    //[30] = ACTION_MODS_TAP_KEY(MOD_RSFT, KC_GRAVE),
-    //[31] = ACTION_MODS_TAP_KEY(MOD_RSFT, KC_BSLASH),
-};
-
-    /*
-     * Layer n: Template
+    /* Layer 2: Vi mode
      * ,-----------------------------------------------------------.
      * |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |
      * |-----------------------------------------------------------|
-     * |     |   |   |   |   |   |   |   |   |   |   |   |   |     |
+     * |     |   |   |   |   |   |Hom|PgD|PgU|End|   |   |   |     |
      * |-----------------------------------------------------------|
-     * |      |   |   |   |   |   |   |   |   |   |   |   |        |
+     * |      |   |   |   |   |   |Lef|Dow|Up |Rig|   |   |        |
      * |-----------------------------------------------------------|
      * |        |   |   |   |   |   |   |   |   |   |   |      |   |
      * `-----------------------------------------------------------'
      *       |   |     |                       |     |   |
      *       `-------------------------------------------'
      */
-// vim: tabstop=4 shiftwidth=4
+    [2] = \
+    KEYMAP(PWR,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,   \
+           TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,HOME,PGDN,PGUP, END,TRNS,TRNS,TRNS, TRNS,      \
+           TRNS,TRNS,TRNS,TRNS, FN4,TRNS,LEFT,DOWN,  UP,RGHT,TRNS,TRNS,TRNS,            \
+           TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,            \
+              TRNS,TRNS,        TRNS,             TRNS,TRNS),
+
+};
+
+
+
+/* id for user defined functions */
+enum function_id {
+    LSHIFT_LPAREN,
+    RSHIFT_RPAREN,
+};
+
+/*
+ * Fn action definition
+ */
+#ifdef KEYMAP_SECTION_ENABLE
+const action_t fn_actions[] __attribute__ ((section (".keymap.fn_actions"))) = {
+};
+#else
+const action_t fn_actions[] PROGMEM = {
+    [0]  = ACTION_LAYER_MOMENTARY(1),
+    [1]  = ACTION_MODS_TAP_KEY(MOD_LCTL, KC_ESC),
+    [2]  = ACTION_FUNCTION_TAP(LSHIFT_LPAREN),
+    [3]  = ACTION_FUNCTION_TAP(RSHIFT_RPAREN),
+    [4] = ACTION_LAYER_TAP_KEY(2, KC_F),
+};
+#endif
+
+
+/*
+ * user defined action function
+ */
+void action_function(keyrecord_t *record, uint8_t id, uint8_t opt)
+{
+    if (record->event.pressed) dprint("P"); else dprint("R");
+    dprintf("%d", record->tap.count);
+    if (record->tap.interrupted) dprint("i");
+    dprint("\n");
+
+    switch (id) {
+        case LSHIFT_LPAREN:
+            // Shift parentheses example: LShft + tap '('
+            // http://stevelosh.com/blog/2012/10/a-modern-space-cadet/#shift-parentheses
+            // http://geekhack.org/index.php?topic=41989.msg1304899#msg1304899
+            if (record->event.pressed) {
+                if (record->tap.count > 0 && !record->tap.interrupted) {
+                    if (record->tap.interrupted) {
+                        dprint("tap interrupted\n");
+                        register_mods(MOD_BIT(KC_LSHIFT));
+                    }
+                } else {
+                    register_mods(MOD_BIT(KC_LSHIFT));
+                }
+            } else {
+                if (record->tap.count > 0 && !(record->tap.interrupted)) {
+                    add_weak_mods(MOD_BIT(KC_LSHIFT));
+                    send_keyboard_report();
+                    register_code(KC_9);
+                    unregister_code(KC_9);
+                    del_weak_mods(MOD_BIT(KC_LSHIFT));
+                    send_keyboard_report();
+                    record->tap.count = 0;  // ad hoc: cancel tap
+                } else {
+                    unregister_mods(MOD_BIT(KC_LSHIFT));
+                }
+            }
+            break;
+        case RSHIFT_RPAREN:
+            // Shift parentheses example: RShft + tap ')'
+            if (record->event.pressed) {
+                if (record->tap.count > 0 && !record->tap.interrupted) {
+                    if (record->tap.interrupted) {
+                        dprint("tap interrupted\n");
+                        register_mods(MOD_BIT(KC_RSHIFT));
+                    }
+                } else {
+                    register_mods(MOD_BIT(KC_RSHIFT));
+                }
+            } else {
+                if (record->tap.count > 0 && !(record->tap.interrupted)) {
+                    add_weak_mods(MOD_BIT(KC_RSHIFT));
+                    send_keyboard_report();
+                    register_code(KC_0);
+                    unregister_code(KC_0);
+                    del_weak_mods(MOD_BIT(KC_RSHIFT));
+                    send_keyboard_report();
+                    record->tap.count = 0;  // ad hoc: cancel tap
+                } else {
+                    unregister_mods(MOD_BIT(KC_RSHIFT));
+                }
+            }
+            break;
+    }
+}
